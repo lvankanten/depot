489b59ad4f161a2b57ac9f1f74a4f8fcedabb457
Les van Kanten <datababy@mac.com>
2013-10-29 12:48:17 -0700

    Depot Scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :price, numbericality: {greater_than
+	validates	:title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+
 end
